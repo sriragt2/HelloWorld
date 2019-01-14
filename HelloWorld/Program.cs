ad2c3ed30803ed76d38a2bce9ac75447ab3ad2d9
srirag ramadugu <srirag.ramadugu@outlook.com>
2019-01-13 21:54:11 -0500

    version2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,6 +8,9 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.ReadKey(true);
+            string text = Console.ReadLine();
+            Console.WriteLine("Hello World!"+text);
+            Console.ReadKey(true);
         }
     }
 }
